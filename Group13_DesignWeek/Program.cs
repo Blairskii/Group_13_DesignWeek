2d94756efaeb2e2650a100084d32f28aaf4b356a
Blairskii <000755278@mohawkcollege.ca>
2025-10-06 14:40:11 -0400

    Initial Commit (sln)

---
+++ Group13_DesignWeek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Group13_DesignWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
